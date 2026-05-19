33ac32f16e88414875ecd9b4a19e7282f827c5de
Blakecode123 <blakemccall93@gmail.com>
2026-05-19 20:29:57 +1000

    Add DEPLOY.bat for one-click manual deploys

---
+++ DEPLOY.bat
@@ -0,0 +1,8 @@
+@echo off
+echo.
+echo  Deploying Anxi website to anxi.com.au...
+echo.
+netlify deploy --prod --dir "G:\My Drive\Luci Website"
+echo.
+echo  Done! Check above for any errors.
+pause
